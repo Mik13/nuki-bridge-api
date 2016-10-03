8b125b0d74bb7cba3751cc663c70471a7d5338bf
Mik13 <Mik13@users.noreply.github.com>
2016-10-03 16:51:34 +0200

    Use `nukiio-dummy-bridge` for testing

--- test/nuki-test.js
+++ test/nuki-test.js
@@ -6,19 +6,25 @@ describe('Nuki Bridge API', function () {
   var bridgeInstance;
   var nuki;
 
+  before(function (done) {
+    // Start dummy bridge
+    require('nukiio-dummy-bridge');
+
+    this.timeout(10000);
+
+    // Wait for the dummy bridge to listen
+    setTimeout(function () {
+      done();
+    }, 5000);
+  });
+
   it('should be able to get bridge instance', function () {
-    bridgeInstance = new API.Bridge('127.0.0.1', 12345, 'token');
+    bridgeInstance = new API.Bridge('127.0.0.1', 8881, 'token');
 
     assert.ok(bridgeInstance);
   });
 
   describe('Bridge Instance', function () {
-    before(function () {
-      bridgeInstance._request = function mockRequest () {
-        return Promise.resolve([{"nukiId": 1, "name": "Home"}, {"nukiId": 2, "name": "Grandma"}]);
-      };
-    });
-
     it('should be able to get a list of connected nukis', function () {
       return bridgeInstance.list().then(function (nukis) {
         assert.ok(nukis);
@@ -33,20 +39,16 @@ describe('Nuki Bridge API', function () {
       var counter = 0;
 
       before(function () {
-        nuki._request = function mockRequest (action) {
-          return Promise.try(function () {
-            if (counter === 0) {
-              return {"state": 1, "stateName": "locked", "batteryCritical": true, success: true};
-            }
+        // Add `batteryCritical`-flag
+        nuki._originalRequest = nuki._request;
+        nuki._request = function mockRequest (action, actionParameter) {
+          if (!actionParameter) {
+            actionParameter = '';
+          }
 
-            if (action === 'lockState') {
-              return {"state": 1, "stateName": "locked", "batteryCritical": false, success: true};
-            }
+          actionParameter += '&batteryCritical=true';
 
-            if (action === 'lockAction') {
-              return {"batteryCritical": false, success: "true"};
-            }
-          });
+          return nuki._originalRequest(action, actionParameter);
         };
       });
 
@@ -57,20 +59,19 @@ describe('Nuki Bridge API', function () {
           batteryCriticalFired = true;
         });
 
-        return nuki.lockState().then(function (result) {
+        return nuki.lockState().then(function () {
           assert.equal(batteryCriticalFired, true);
-          assert.equal(result, API.lockState.LOCKED);
         });
+      });
+
+      it('should do lock action', function () {
+        return nuki.lockAction(API.lockAction.LOCK);
       });
 
       it('should get lock state', function () {
         return nuki.lockState().then(function (result) {
           assert.equal(result, API.lockState.LOCKED);
         });
-      });
-
-      it('should do lock action', function () {
-        return nuki.lockAction(API.lockAction.UNLOCK);
       });
     });
   });
